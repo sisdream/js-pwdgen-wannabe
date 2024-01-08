37b92001418e5c0aad28f85ce7548b934147b7b5
sisdream <sissipascale2@gmail.com>
2024-01-08 15:43:15 +0100

    feat: variabili js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//NOME
+const FirstName = prompt("Inserisci il tuo nome");
+console.log(FirstName);
+
+//COGNOME
+const Surname = prompt("Inserisci il tuo cognome");
+console.log(Surname);
+
+//COLORE PREFERITO
