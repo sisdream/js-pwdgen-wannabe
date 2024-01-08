005e9df806c99d1f27786bf0a86e0117199be92a
sisdream <sissipascale2@gmail.com>
2024-01-08 16:28:42 +0100

    fix: var name

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ const firstName = prompt("Inserisci il tuo nome");
 console.log(firstName);
 
 //COGNOME
-const surName = prompt("Inserisci il tuo cognome");
-console.log(surName);
+const surname = prompt("Inserisci il tuo cognome");
+console.log(surname);
 
 //COLORE PREFERITO
 const favColor = prompt("Inserisci il tuo colore preferito");
 console.log(favColor);
 
 //CREAZIONE PASSWORD
-const passWord = firstName + surName + favColor + "23";
+const passWord = firstName + surname + favColor + "23";
 console.log(passWord);
 creatPass.innerHTML = `La tua password è <mark> ${passWord} </mark>`; 
