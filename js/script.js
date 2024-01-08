b78a5a39642d4618d25e7267d372ce0a21ff0699
sisdream <sissipascale2@gmail.com>
2024-01-08 16:02:56 +0100

    fix: variabile

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
+const CreatPass = document.getElementById("password")
+
 //NOME
 const FirstName = prompt("Inserisci il tuo nome");
 console.log(FirstName);
 
 //COGNOME
-const Surname = prompt("Inserisci il tuo cognome");
-console.log(Surname);
+const SurName = prompt("Inserisci il tuo cognome");
+console.log(SurName);
 
 //COLORE PREFERITO
+const FavColor = prompt("Inserisci il tuo colore preferito");
+console.log(FavColor);
+
+//CREAZIONE PASSWORD
+const PassWord = FirstName + SurName + FavColor + "23";
+console.log(PassWord);
+CreatPass.innerHTML = `La tua password è <mark> ${PassWord} </mark>`; 
